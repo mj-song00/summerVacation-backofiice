7913165435e8f1efd30c2a4ae8e418cf4693d595
mj-song00 <niveus128@gmail.com>
2024-03-24 22:11:48 +0900

    Feat: admin Entity 생성

--- src/admin/entities/admin.entity.ts
+++ src/admin/entities/admin.entity.ts
@@ -1,1 +1,13 @@
-export class Admin {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Admin {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
