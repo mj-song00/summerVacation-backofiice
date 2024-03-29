e62098049276c66372c5466dfc8ac8dff9043d7f
mj-song00 <niveus128@gmail.com>
2024-03-29 13:26:53 +0900

    Feat: role Decorator add

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { ROLE } from './admin.enum';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 
@@ -15,6 +17,7 @@ export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Post('/register')
+  @Roles(ROLE.USER)
   create(@Body() createAdminDto: CreateAdminDto) {
     return this.adminService.create(createAdminDto);
   }
