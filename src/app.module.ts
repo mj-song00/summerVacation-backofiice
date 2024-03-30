a54706d3b0a0b9f8f0fd4d5cbcccaa989c102ff7
mj-song00 <niveus128@gmail.com>
2024-03-30 13:37:49 +0900

    Feat: add diary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { DiaryModule } from './diary/diary.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UserModule } from './user/user.module';
     DatabaseModule,
     AdminModule,
     UserModule,
+    DiaryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
